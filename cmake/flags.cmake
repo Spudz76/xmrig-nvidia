a3a14bcbc5680d0e6e891534c92697b3204e6511
Tony Butler <spudz76@gmail.com>
2019-05-03 20:22:31 -0600

    Intel ICC and ROTR detection patches

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -1,3 +1,7 @@
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+set(CMAKE_CXX_STANDARD 11)
+
 if ("${CMAKE_BUILD_TYPE}" STREQUAL "")
     set(CMAKE_BUILD_TYPE Release)
 endif()
@@ -6,9 +10,19 @@ if (CMAKE_BUILD_TYPE STREQUAL "Release")
     add_definitions(/DNDEBUG)
 endif()
 
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_CXX_EXTENSIONS OFF)
-set(CMAKE_CXX_STANDARD 11)
+include(CheckSymbolExists)
+set(CMAKE_REQUIRED_QUIET TRUE)
+message(STATUS "Looking for _rotr in <x86intrin.h>...")
+check_symbol_exists("_rotr" "x86intrin.h" HAVE_X86ROTR)
+if (NOT HAVE_X86ROTR)
+    message(STATUS "Looking for _rotr in <intrin.h>...")
+    check_symbol_exists("_rotr" "intrin.h" HAVE_ROTR)
+endif()
+if (HAVE_X86ROTR OR HAVE_ROTR)
+    message(STATUS "Looking for _rotr - found")
+    add_definitions(/DHAVE_ROTR)
+endif()
+unset(CMAKE_REQUIRED_QUIET)
 
 if (CMAKE_CXX_COMPILER_ID MATCHES GNU)
 
@@ -61,4 +75,25 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES Clang)
 
     set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -s")
 
+elseif (CMAKE_CXX_COMPILER_ID MATCHES Intel)
+
+    if(("${ICC_COMPILERS}" STREQUAL "") AND (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 17) AND (CMAKE_CXX_COMPILER_VERSION VERSION_LESS 20))
+        # locate best gcc version to pass to icc 18 or 19 (gcc-8 doesn't work, for example)
+        # Use -DICC_GCCVER=6 for example to force 'gcc-6' and 'g++-6'
+        # Otherwise, the newest available and compatible pair will be selected
+        find_program(ICC_GXX NAMES "g++-${ICC_GCCVER}" "g++-7" "g++-6" "g++")
+        get_filename_component(ICC_GXX ${ICC_GXX} NAME)
+        find_program(ICC_GCC NAMES "gcc-${ICC_GCCVER}" "gcc-7" "gcc-6" "gcc")
+        get_filename_component(ICC_GCC ${ICC_GCC} NAME)
+        set(ICC_COMPILERS "-gxx-name=${ICC_GXX} -gcc-name=${ICC_GCC}")
+        unset(ICC_GXX)
+        unset(ICC_GCC)
+    endif()
+    message(STATUS "Intel ICC subcompiler flags: ${ICC_COMPILERS}")
+
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ICC_COMPILERS} -Wall")
+    set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS_RELEASE} -funroll-loops -xHost -ipo -msse -msse2 -mfpmath=sse")
+
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ICC_COMPILERS} -Wall -fno-exceptions -fno-rtti")
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} -funroll-loops -xHost -ipo -msse -msse2 -mfpmath=sse")
 endif()
